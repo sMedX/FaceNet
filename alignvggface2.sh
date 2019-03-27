5e9096048453354b80b14e59c1b5c5e9f53fe9a5
kosarev@smedx.com <kosarev@smedx.com>
2019-03-27 14:15:18 +0300

    bash scripts have been updated

---
+++ alignvggface2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+echo "align VGGFace2 datasets using mtcnn"
+
+python3.5m -m facenet.align.align_dataset_mtcnn \
+    ~/datasets/vggface2/train/ \
+    ~/datasets/vggface2/vggface2_train_mtcnnalign_182 \
+    --gpu_memory_fraction 1.0 \
+    --image_size 182 \
+    --margin 44
+
+python3.5m -m facenet.align.align_dataset_mtcnn \
+    ~/datasets/vggface2/test/ \
+    ~/datasets/vggface2/vggface2_test_mtcnnalign_182 \
+    --gpu_memory_fraction 1.0 \
+    --image_size 182 \
+    --margin 44
+
