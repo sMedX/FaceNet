de758f89b83f1a8fff9732ee5927ebe18b3bda62
kosarev@smedx.com <kosarev@smedx.com>
2019-03-27 14:36:33 +0300

    bag fixed

--- alignlfw.sh
+++ alignlfw.sh
@@ -6,5 +6,5 @@ python3.5m -m facenet.align.align_dataset_mtcnn \
     ~/datasets/lfw/lfw_mtcnnalign_160 \
     --gpu_memory_fraction 1.0 \
     --image_size 160 \
-    --margin 32
+    --margin 32 \
     --random_order \
