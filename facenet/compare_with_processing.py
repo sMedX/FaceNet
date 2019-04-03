a3e8b65d122dd1d09de864f6ab3bad2cb31b9c55
kosarev@smedx.com <kosarev@smedx.com>
2019-04-03 17:43:49 +0300

    application compare_with_processing has been added

---
+++ facenet/compare_with_processing.py
@@ -0,0 +1,163 @@
+"""Performs face alignment and calculates L2 distance between the embeddings of images."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from scipy import misc
+from PIL import Image
+import tensorflow as tf
+import numpy as np
+import sys
+import os
+import copy
+import argparse
+from facenet.align import detect_face
+from facenet import facenet
+
+minsize = 20  # minimum size of face
+threshold = (0.6, 0.7, 0.7)  # three steps's threshold
+factor = 0.709  # scale factor
+
+
+def main(args):
+
+    images1 = load_and_align_data(args.image_files, args.image_size, args.margin, args.gpu_memory_fraction)
+    images2 = load_and_align_data_with_processing(args.image_files, args.image_size, args.margin, args.gpu_memory_fraction, args)
+
+    with tf.Graph().as_default():
+        with tf.Session() as sess:
+      
+            # Load the model
+            facenet.load_model(args.model)
+    
+            # Get input and output tensors
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+
+            # Run forward pass to calculate embeddings
+            feed_dict = {images_placeholder: images1, phase_train_placeholder: False}
+            emb1 = sess.run(embeddings, feed_dict=feed_dict)
+
+            feed_dict = {images_placeholder: images2, phase_train_placeholder: False}
+            emb2 = sess.run(embeddings, feed_dict=feed_dict)
+
+            nrof_images = len(args.image_files)
+
+            print('Images:')
+            for i in range(nrof_images):
+                print('%1d: %s' % (i, args.image_files[i]))
+            print('')
+            
+            # Print distance matrix
+            print('Distancies')
+            print('')
+            for i in range(nrof_images):
+                print('%1d  ' % i, end='')
+                dist = np.sqrt(np.sum(np.square(np.subtract(emb1[i, :], emb2[i, :]))))
+                print('  %1.4f  ' % dist, end='')
+                print('')
+            
+            
+def load_and_align_data(image_paths, image_size, margin, gpu_memory_fraction):
+
+    print('Creating networks and loading parameters')
+    with tf.Graph().as_default():
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        with sess.as_default():
+            pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
+  
+    tmp_image_paths = copy.copy(image_paths)
+    img_list = []
+
+    for image in tmp_image_paths:
+        img = misc.imread(os.path.expanduser(image), mode='RGB')
+        img_size = np.asarray(img.shape)[0:2]
+        bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+
+        if len(bounding_boxes) < 1:
+            image_paths.remove(image)
+            print("can't detect face, remove", image)
+            continue
+
+        det = np.squeeze(bounding_boxes[0, 0:4])
+        bb = np.zeros(4, dtype=np.int32)
+        bb[0] = np.maximum(det[0]-margin/2, 0)
+        bb[1] = np.maximum(det[1]-margin/2, 0)
+        bb[2] = np.minimum(det[2]+margin/2, img_size[1])
+        bb[3] = np.minimum(det[3]+margin/2, img_size[0])
+        cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
+        aligned = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
+        prewhitened = facenet.prewhiten(aligned)
+        img_list.append(prewhitened)
+
+    images = np.stack(img_list)
+
+    return images
+
+
+def load_and_align_data_with_processing(image_paths, image_size, margin, gpu_memory_fraction, args):
+    print('Creating networks and loading parameters')
+    with tf.Graph().as_default():
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        with sess.as_default():
+            pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
+
+    print('Parameters for image processing')
+    print('rotation angle in degrees', args.rotation)
+
+    tmp_image_paths = copy.copy(image_paths)
+    img_list = []
+
+    for image in tmp_image_paths:
+        img = Image.open(os.path.expanduser(image))
+        img = img.rotate(args.rotation)
+        img = np.array(img)
+
+        img_size = np.asarray(img.shape)[0:2]
+        bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+
+        if len(bounding_boxes) < 1:
+            image_paths.remove(image)
+            print("can't detect face, remove", image)
+            continue
+
+        det = np.squeeze(bounding_boxes[0, 0:4])
+        bb = np.zeros(4, dtype=np.int32)
+        bb[0] = np.maximum(det[0] - margin / 2, 0)
+        bb[1] = np.maximum(det[1] - margin / 2, 0)
+        bb[2] = np.minimum(det[2] + margin / 2, img_size[1])
+        bb[3] = np.minimum(det[3] + margin / 2, img_size[0])
+        cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
+        aligned = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
+        prewhitened = facenet.prewhiten(aligned)
+        img_list.append(prewhitened)
+
+    images = np.stack(img_list)
+
+    return images
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('model', type=str,
+        help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file')
+    parser.add_argument('image_files', type=str, nargs='+', help='Images to compare')
+    parser.add_argument('--rotation', type=float,
+        help='Rotation angle in degrees.', default=0)
+    parser.add_argument('--image_size', type=int,
+        help='Image size (height, width) in pixels.', default=160)
+    parser.add_argument('--margin', type=int,
+        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
+    parser.add_argument('--gpu_memory_fraction', type=float,
+        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
+
+    return parser.parse_args(argv[1:])
+
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv))
