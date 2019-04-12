18d4bea5ad1c4745867a147fc5b95949a43ce514
kosarev@smedx.com <kosarev@smedx.com>
2019-04-12 13:46:38 +0300

    default path for pretrained models updated

--- validate_on_lfw.sh
+++ validate_on_lfw.sh
@@ -11,7 +11,7 @@ echo "model directory" ${md}
 
 python3 -m facenet.validate_on_lfw \
     ${ds}/datasets/lfw_mtcnnalign_160 \
-    ${md}/facenet/softmax/models/20190327-175738 \
+    ${md}/models/facenet/pretrained/20190327-175738 \
     --distance_metric 1 \
     --use_flipped_images \
     --subtract_mean \
