15d9e33fa42bec9d451d0ed3854eb5f2dcf8e40a
kosarev@smedx.com <kosarev@smedx.com>
2019-04-18 16:06:33 +0300

    function label_matrix() has been added

--- facenet/utils.py
+++ facenet/utils.py
@@ -1,5 +1,6 @@
 
 import os
+import numpy as np
 
 
 def get_files(dirname):
@@ -9,3 +10,21 @@ def get_files(dirname):
         list_of_files += [os.path.join(root, file) for file in files]
 
     return list_of_files
+
+
+def label_matrix(image_paths, diagonal=True):
+
+    basenames = [os.path.basename(os.path.dirname(path)) for path in image_paths]
+
+    labels = np.zeros([len(image_paths), len(image_paths)], dtype=np.uint8)
+
+    for i, basename1 in enumerate(basenames):
+        for k, basename2 in enumerate(basenames[:i]):
+            if basename1 == basename2:
+                labels[i][k] = labels[k][i] = 1
+
+        if diagonal:
+            labels[i][i] = 1
+
+    return labels
+
