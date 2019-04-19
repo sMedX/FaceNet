5999c6f26b472fef3e4194bf9002b7910fd926c8
kosarev@smedx.com <kosarev@smedx.com>
2019-04-19 17:35:23 +0300

    update path in bash script

--- validate.sh
+++ validate.sh
@@ -11,7 +11,7 @@ echo "model directory" ${md}
 
 python3 -m facenet.validate \
     ${md}/models/facenet/pretrained/20190327-175738 \
-    ${ds}/datasets/megaface/megaface_mtcnnalign_160 \
+    ${ds}/datasets/megaface/megaface_mtcnnaligned_160 \
     --nrof_folders 0 \
     --distance_metric 0 \
     --subtract_mean \
