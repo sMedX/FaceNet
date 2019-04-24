e299036054b3032e9e3cd72975c91e0de61e3383
kosarev@smedx.com <kosarev@smedx.com>
2019-04-25 00:42:27 +0300

    update bash script

--- validate.sh
+++ validate.sh
@@ -15,5 +15,5 @@ python3 -m facenet.validate \
     --nrof_folders 0 \
     --distance_metric 0 \
     --use_fixed_image_standardization \
-    --false_positive_dir ${ds}/megaface_false_positive_pairs \
-    --false_negative_dir ${ds}/megaface_false_negative_pairs
+    --false_positive_dir ${ds}/datasets/megaface_false_positive_pairs \
+    --false_negative_dir ${ds}/datasets/megaface_false_negative_pairs
