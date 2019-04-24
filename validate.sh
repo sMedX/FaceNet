f747cab5b3907a29ca1d68c70447205cfb093643
kosarev@smedx.com <kosarev@smedx.com>
2019-04-25 00:34:45 +0300

    update bash script

--- validate.sh
+++ validate.sh
@@ -12,9 +12,8 @@ echo "model directory" ${md}
 python3 -m facenet.validate \
     ${md}/models/facenet/pretrained/20190410-013706 \
     ${ds}/datasets/megaface/megaface_mtcnnaligned_160 \
-    --nrof_folders 0 \
+    --nrof_folders 400 \
     --distance_metric 0 \
-    --subtract_mean \
     --use_fixed_image_standardization \
     --false_positive_dir ${ds}/megaface_false_positive_pairs \
     --false_negative_dir ${ds}/megaface_false_negative_pairs
