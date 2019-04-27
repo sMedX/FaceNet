d06f2bcc6747ba55bec5e605bb4195ffc937fb80
kosarev@smedx.com <kosarev@smedx.com>
2019-04-27 09:31:49 +0300

    functions to write tfrecord file have been implemented

--- facenet/utils.py
+++ facenet/utils.py
@@ -3,6 +3,7 @@ import os
 from subprocess import Popen, PIPE
 import numpy as np
 from scipy import spatial
+import tensorflow as tf
 
 
 def label_array(labels):
@@ -49,11 +50,50 @@ def git_hash():
 
     return git_hash, git_diff
 
-    # Store a text file in the log directory
-    rev_info_filename = os.path.join(output_dir, 'revision_info.txt')
-    with open(rev_info_filename, "w") as text_file:
-        text_file.write('arguments: %s\n--------------------\n' % arg_string)
-        text_file.write('tensorflow version: %s\n--------------------\n' % tf.__version__)  # @UndefinedVariable
-        text_file.write('git hash: %s\n--------------------\n' % git_hash)
-        text_file.write('%s' % git_diff)
+
+def int64_feature(value):
+    """Wrapper for insert int64 feature into Example proto."""
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def float_feature(value):
+    """Wrapper for insert float features into Example proto."""
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+def bytes_feature(value):
+    """Wrapper for insert bytes features into Example proto."""
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
+
+def write_tfrecord(tfrecord, files, embeddings):
+
+    # tf record file name
+    if os.path.isfile(tfrecord):
+        os.remove(tfrecord)
+
+    with tf.python_io.TFRecordWriter(tfrecord) as tfwriter:
+        for i, (file, embedding) in enumerate(zip(files, embeddings)):
+            add_to_tfrecord(tfwriter, file.encode(), embedding.tostring())
+
+            if (i+1) % 100 == 0:
+                print('\r{}/{} samples have been added to tfrecord file.'.format(i+1, len(files)), end='')
+
+    print('\rtfrecord file {} has been written, number of samples is {}.'.format(tfrecord, len(files)))
+
+
+def add_to_tfrecord(tfwriter, file, embedding):
+    example = tf.train.Example(
+        features=tf.train.Features(feature={
+            'filename': bytes_feature(file),
+            'embedding': bytes_feature(embedding)
+            }))
+
+    tfwriter.write(example.SerializeToString())
 
