b0a578c579ba58cf9c60b39bc6c3ab79be821f18
kosarev@smedx.com <kosarev@smedx.com>
2019-07-18 21:47:03 +0300

    update sh script

--- extract_faces_vggface2.sh
+++ extract_faces_vggface2.sh
@@ -5,13 +5,9 @@ size=182
 margin=0.2
 detector=frcnnv3
 
-for N in {1..4}; do
 python apps/extract_faces.py \
  ~/datasets/vggface2/train/ \
  ~/datasets/vggface2/train_${detector}extracted_${size} \
  --detector ${detector} \
  --image_size ${size} \
  --margin ${margin} \
- --gpu_memory_fraction 0.25 \
- --random_order \
-& done
