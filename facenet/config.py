c37ade7952615ccebff40d30c8999485c51998e2
kosarev@smedx.com <kosarev@smedx.com>
2019-07-21 13:06:59 +0300

    error fixed

--- facenet/config.py
+++ facenet/config.py
@@ -1,14 +1,17 @@
 # coding: utf-8
 __author__ = 'Ruslan N. Kosarev'
 
+import os
 import pathlib as plib
+
+dirname = os.path.dirname(__file__)
 
 
 class DefaultConfig:
     def __init__(self):
-        self.pretrained_model = plib.Path(__file__).parent.joinpath('models',
-                                                                    '20190410-013706',
-                                                                    '20190410-013706.pb')
+        self.pretrained_model = plib.Path(dirname).joinpath('models',
+                                                            '20190410-013706',
+                                                            '20190410-013706.pb')
 
         # type of distance metric to use. 0: Euclidian, 1:Cosine similarity distance
         self.distance_metric = 1
