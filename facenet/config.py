175ae09bbd8ed13e4e9d875e6dd147391a542777
kosarev@smedx.com <kosarev@smedx.com>
2019-08-01 11:46:09 +0300

    default facenet has been set to 20190727-080213

--- facenet/config.py
+++ facenet/config.py
@@ -9,7 +9,7 @@ basedir = plib.Path(__file__).parents[1]
 
 class DefaultConfig:
     def __init__(self):
-        self.model = str(basedir.joinpath('models', '20190721-142131'))
+        self.model = str(basedir.joinpath('models', '20190727-080213'))
 
         # type of distance metric to use. 0: Euclidian, 1:Cosine similarity distance
         self.distance_metric = 0
