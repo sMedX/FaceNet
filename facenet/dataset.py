af1d4c91ff53eee5d6754485e13c6f79fafb7ae0
kosarev@smedx.com <kosarev@smedx.com>
2019-08-08 17:59:36 +0300

    refactoring application embeddings.py

--- facenet/dataset.py
+++ facenet/dataset.py
@@ -3,6 +3,47 @@ import os
 import numpy as np
 
 
+class ImageClass:
+    """
+    Stores the paths to images for a given class
+    """
+    def __init__(self, name, image_paths):
+        self.name = name
+        self.image_paths = image_paths
+
+    def __str__(self):
+        return self.name + ', ' + str(len(self.image_paths)) + ' images'
+
+    def __len__(self):
+        return len(self.image_paths)
+
+
+def get_image_paths(facedir):
+    image_paths = []
+    if os.path.isdir(facedir):
+        images = os.listdir(facedir)
+        image_paths = [os.path.join(facedir, img) for img in images]
+    return image_paths
+
+
+def dataset(path, nrof_classes=0, has_class_directories=True):
+    ds = []
+    path_exp = os.path.expanduser(path)
+    classes = [path for path in os.listdir(path_exp) if os.path.isdir(os.path.join(path_exp, path))]
+
+    if nrof_classes > 0:
+        classes = classes[:nrof_classes]
+
+    nrof_classes = len(classes)
+    for i in range(nrof_classes):
+        class_name = classes[i]
+        facedir = os.path.join(path_exp, class_name)
+        image_paths = get_image_paths(facedir)
+        ds.append(ImageClass(class_name, image_paths))
+
+    return ds
+
+
 class Dataset:
     def __init__(self, dirname, nrof_folders=0):
         self.dirname = os.path.expanduser(dirname)
