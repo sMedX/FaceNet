362c0f1b5f8999e493c0e19fc911d3c1e55df32c
kosarev@smedx.com <kosarev@smedx.com>
2019-08-09 13:33:25 +0300

    commented lines have been deleted

--- facenet/apps/cleanup.py
+++ facenet/apps/cleanup.py
@@ -83,24 +83,6 @@ def parse_arguments(argv):
         help='Directory to save examples.', default=None)
     parser.add_argument('--h5file', type=str,
         help='Path to h5 file to save information about false images.', default=None)
-
-
-    # parser.add_argument('tfrecord', type=str,
-    #     help='Path to tfrecord file with embeddings.')
-    # parser.add_argument('--threshold', type=float,
-    #     help='Threshold value to identify same faces.', default=1)
-    # parser.add_argument('--distance_metric', type=int,
-    #     help='Distance metric  0:euclidian, 1:cosine similarity.', default=0)
-    # parser.add_argument('--subtract_mean',
-    #     help='Subtract feature mean before calculating distance.', action='store_true')
-    # parser.add_argument('--false_positive_dir', type=str,
-    #     help='Directory to save false positive pairs.', default='')
-    # parser.add_argument('--nrof_fpos_images', type=int,
-    #     help='Number of false positive pairs per folder.', default=10)
-    # parser.add_argument('--false_negative_dir', type=str,
-    #     help='Directory to save false negative pairs.', default='')
-    # parser.add_argument('--nrof_fneg_images', type=int,
-    #     help='Number of false negative pairs per folder.', default=2)
     return parser.parse_args(argv[1:])
 
 
