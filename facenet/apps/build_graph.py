ab6275e27311273585e45f54c62d53df9d124c8a
korus <ruslan.kosarev@gmail.com>
2019-12-05 14:28:40 +0300

    printing operations added

--- facenet/apps/build_graph.py
+++ facenet/apps/build_graph.py
@@ -26,17 +26,18 @@ def main(**args_):
     if args.model_config is None:
         args.update_from_file(network.config_file)
 
+    # Build the inference graph
     with tf.Graph().as_default():
         image_batch = tf.convert_to_tensor(np.zeros([1, 160, 160, 3]), np.float32)
         image_batch = tf.identity(image_batch, 'input')
 
-        # Build the inference graph
         print('Building training graph')
         prelogits, _ = network.inference(image_batch,
                                          keep_probability=args.model_config.keep_probability,
                                          phase_train=False,
                                          weight_decay=args.model_config.weight_decay,
                                          config=args.model_config)
+
         embeddings = tf.nn.l2_normalize(prelogits, 1, 1e-10, name='embeddings')
 
         # start running operations on the graph.
@@ -49,6 +50,11 @@ def main(**args_):
             writer = tf.summary.FileWriter(args_['logs'], sess.graph)
             sess.run(embeddings)
             writer.close()
+
+        for i, op in enumerate(tf.get_default_graph().get_operations()):
+            print('{}/{}'.format(i, len(tf.get_default_graph().get_operations())), op.name, op.type)
+            print('\tinputs ', op.inputs)
+            print('\toutputs', op.outputs)
 
 
 if __name__ == '__main__':
