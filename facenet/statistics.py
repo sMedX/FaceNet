0e89998b549fac6d50a1397529710f1f2d532e4c
korus <ruslan.kosarev@gmail.com>
2020-01-20 10:40:46 +0300

    refactoring

--- facenet/statistics.py
+++ facenet/statistics.py
@@ -234,10 +234,11 @@ class Validation:
             accuracy_threshold = thresholds[np.argmax(conf_matrix.accuracy)]
 
             # find the threshold that gives FAR (FPR, 1-TNR) = far_target
-            far_thresholds = 0.0
             if np.max(conf_matrix.fp_rates) >= self.far_target:
                 f = interpolate.interp1d(conf_matrix.fp_rates, thresholds, kind='slinear')
-                far_thresholds = f(self.far_target)
+                far_threshold = f(self.far_target)
+            else:
+                far_threshold = 0.0
 
             # evaluations with test set
             distances = compute_distances(self.embeddings[test_set], self.labels[test_set], metric=0)
@@ -248,7 +249,7 @@ class Validation:
             # self.tn_rates[fold_idx] = conf_matrix.tn_rates
             self.report_acc.append_fold('test', conf_matrix)
 
-            conf_matrix = ConfidenceMatrix(distances, far_thresholds)
+            conf_matrix = ConfidenceMatrix(distances, far_threshold)
             self.report_far.append_fold('test', conf_matrix)
             # self.accuracy_far_target[fold_idx] = conf_matrix.accuracy
             # self.precision_far_target[fold_idx] = conf_matrix.precision
