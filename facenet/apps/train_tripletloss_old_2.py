c516343f02e944bf0c2da0b0e29ebec6258a182b
korus <ruslan.kosarev@gmail.com>
2020-02-03 17:56:29 +0300

    updates

--- facenet/apps/train_tripletloss_old_2.py
+++ facenet/apps/train_tripletloss_old_2.py
@@ -333,7 +333,7 @@ def sample_people(dataset, people_per_batch, images_per_person):
         np.random.shuffle(image_indices)
         nrof_images_from_class = min(nrof_images_in_class, images_per_person, nrof_images - len(image_paths))
         idx = image_indices[0:nrof_images_from_class]
-        image_paths_for_class = [dataset[class_index].image_paths[j] for j in idx]
+        image_paths_for_class = [dataset.classes[class_index].files[j] for j in idx]
         sampled_class_indices += [class_index] * nrof_images_from_class
         image_paths += image_paths_for_class
         num_per_class.append(nrof_images_from_class)
