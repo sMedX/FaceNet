b50979d3b58c24936bd1e998c6f3d7c68c92c795
korus <ruslan.kosarev@gmail.com>
2020-02-03 21:17:05 +0300

    obsolete functions were deleted

--- facenet/apps/train_tripletloss.py
+++ facenet/apps/train_tripletloss.py
@@ -188,7 +188,7 @@ def main(**args_):
                     break
 
                 # Save variables and the metagraph if it doesn't exist already
-                save_variables_and_metagraph(sess, saver, summary_writer, args.model.path, subdir, step)
+                facenet.save_variables_and_metagraph(sess, saver, summary_writer, args.model.path, subdir, step)
 
                 # Evaluate on LFW
                 # if args.lfw_dir:
@@ -410,114 +410,6 @@ def evaluate(sess, image_paths, embeddings, labels_batch, image_paths_placeholde
     summary_writer.add_summary(summary, step)
     with open(os.path.join(log_dir, 'lfw_result.txt'), 'at') as f:
         f.write('%d\t%.5f\t%.5f\n' % (step, np.mean(accuracy), val))
-
-
-def save_variables_and_metagraph(sess, saver, summary_writer, model_dir, model_name, step):
-    # Save the model checkpoint
-    print('Saving variables')
-    start_time = time.time()
-    checkpoint_path = os.path.join(model_dir, 'model-%s.ckpt' % model_name)
-    saver.save(sess, checkpoint_path, global_step=step, write_meta_graph=False)
-    save_time_variables = time.time() - start_time
-    print('Variables saved in %.2f seconds' % save_time_variables)
-    metagraph_filename = os.path.join(model_dir, 'model-%s.meta' % model_name)
-    save_time_metagraph = 0
-    if not os.path.exists(metagraph_filename):
-        print('Saving metagraph')
-        start_time = time.time()
-        saver.export_meta_graph(metagraph_filename)
-        save_time_metagraph = time.time() - start_time
-        print('Metagraph saved in %.2f seconds' % save_time_metagraph)
-    summary = tf.Summary()
-    # pylint: disable=maybe-no-member
-    summary.value.add(tag='time/save_variables', simple_value=save_time_variables)
-    summary.value.add(tag='time/save_metagraph', simple_value=save_time_metagraph)
-    summary_writer.add_summary(summary, step)
-
-
-def get_learning_rate_from_file(filename, epoch):
-    with open(filename, 'r') as f:
-        for line in f.readlines():
-            line = line.split('#', 1)[0]
-            if line:
-                par = line.strip().split(':')
-                e = int(par[0])
-                lr = float(par[1])
-                if e <= epoch:
-                    learning_rate = lr
-                else:
-                    return learning_rate
-
-
-def parse_arguments(argv):
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--logs_base_dir', type=str,
-                        help='Directory where to write event logs.', default='~/logs/facenet')
-    parser.add_argument('--models_base_dir', type=str,
-                        help='Directory where to write trained models and checkpoints.', default='~/models/facenet')
-    parser.add_argument('--gpu_memory_fraction', type=float,
-                        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
-    parser.add_argument('--pretrained_model', type=str,
-                        help='Load a pretrained model before training starts.')
-    parser.add_argument('--data_dir', type=str,
-                        help='Path to the data directory containing aligned face patches.',
-                        default='~/datasets/casia/casia_maxpy_mtcnnalign_182_160')
-    parser.add_argument('--model_def', type=str,
-                        help='Model definition. Points to a module containing the definition of the inference graph.',
-                        default='models.inception_resnet_v1')
-    parser.add_argument('--max_nrof_epochs', type=int,
-                        help='Number of epochs to run.', default=500)
-    parser.add_argument('--batch_size', type=int,
-                        help='Number of images to process in a batch.', default=90)
-    parser.add_argument('--image_size', type=int,
-                        help='Image size (height, width) in pixels.', default=160)
-    parser.add_argument('--people_per_batch', type=int,
-                        help='Number of people per batch.', default=45)
-    parser.add_argument('--images_per_person', type=int,
-                        help='Number of images per person.', default=40)
-    parser.add_argument('--epoch_size', type=int,
-                        help='Number of batches per epoch.', default=1000)
-    parser.add_argument('--alpha', type=float,
-                        help='Positive to negative triplet distance margin.', default=0.2)
-    parser.add_argument('--embedding_size', type=int,
-                        help='Dimensionality of the embedding.', default=128)
-    parser.add_argument('--random_crop',
-                        help='Performs random cropping of training images. If false, the center image_size pixels from the training images are used. ' +
-                             'If the size of the images in the data directory is equal to image_size no cropping is performed',
-                        action='store_true')
-    parser.add_argument('--random_flip',
-                        help='Performs random horizontal flipping of training images.', action='store_true')
-    parser.add_argument('--keep_probability', type=float,
-                        help='Keep probability of dropout for the fully connected layer(s).', default=1.0)
-    parser.add_argument('--weight_decay', type=float,
-                        help='L2 weight regularization.', default=0.0)
-    parser.add_argument('--optimizer', type=str, choices=['ADAGRAD', 'ADADELTA', 'ADAM', 'RMSPROP', 'MOM'],
-                        help='The optimization algorithm to use', default='ADAGRAD')
-    parser.add_argument('--learning_rate', type=float,
-                        help='Initial learning rate. If set to a negative value a learning rate ' +
-                             'schedule can be specified in the file "learning_rate_schedule.txt"', default=0.1)
-    parser.add_argument('--learning_rate_decay_epochs', type=int,
-                        help='Number of epochs between learning rate decay.', default=100)
-    parser.add_argument('--learning_rate_decay_factor', type=float,
-                        help='Learning rate decay factor.', default=1.0)
-    parser.add_argument('--moving_average_decay', type=float,
-                        help='Exponential decay for tracking of training parameters.', default=0.9999)
-    parser.add_argument('--seed', type=int,
-                        help='Random seed.', default=666)
-    parser.add_argument('--learning_rate_schedule_file', type=str,
-                        help='File containing the learning rate schedule that is used when learning_rate is set to to -1.',
-                        default='data/learning_rate_schedule.txt')
-    parser.add_argument('--config', type=Path, default='train_tripletloss.yaml')
-
-    # Parameters for validation on LFW
-    parser.add_argument('--lfw_pairs', type=str,
-                        help='The file containing the pairs to use for validation.', default='data/pairs.txt')
-    parser.add_argument('--lfw_dir', type=str,
-                        help='Path to the data directory containing aligned face patches.', default='')
-    parser.add_argument('--lfw_nrof_folds', type=int,
-                        help='Number of folds to use for cross validation. Mainly used for testing.', default=10)
-    return parser.parse_args(argv)
 
 
 if __name__ == '__main__':
