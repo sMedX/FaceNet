6589c81bbac20975a0adf00f78b0beb71d307892
korus <ruslan.kosarev@gmail.com>
2020-02-05 12:16:36 +0300

    refactoring

--- facenet/facenet.py
+++ facenet/facenet.py
@@ -36,7 +36,7 @@ import re
 from tensorflow.python.platform import gfile
 from tensorflow.python.ops import data_flow_ops
 import math
-import pathlib
+from pathlib import Path
 
 from facenet import utils
 
@@ -48,13 +48,6 @@ class Placeholders:
     labels = None
     control = None
     learning_rate = None
-
-
-def restore_checkpoint(saver, session, path):
-    if path is not None:
-        path = pathlib.Path(path)
-        print('Restoring pre-trained model: {}'.format(path))
-        saver.restore(session, str(path))
 
 
 def triplet_loss(anchor, positive, negative, alpha):
@@ -312,95 +305,18 @@ def get_triplet_batch(triplets, batch_index, batch_size):
     return batch
 
 
-def get_learning_rate_from_file(filename, epoch, default_learning_rate=-1.0):
-    learning_rate = default_learning_rate
-
-    with open(filename, 'r') as f:
-        for line in f.readlines():
-            line = line.split('#', 1)[0]
-            if line:
-                par = line.strip().split(':')
-                try:
-                    e = int(par[0])
-                except ValueError:
-                    continue
-
-                if e <= epoch:
-                    try:
-                        learning_rate = float(par[1])
-                    except ValueError:
-                        learning_rate = default_learning_rate
-
-    return learning_rate
-
-
-# class ImageClass:
-#     "Stores the paths to images for a given class"
-#     def __init__(self, name, image_paths):
-#         self.name = name
-#         self.image_paths = image_paths
-#
-#     def __str__(self):
-#         return self.name + ', ' + str(len(self.image_paths)) + ' images'
-#
-#     def __len__(self):
-#         return len(self.image_paths)
-  
-
-# def get_dataset(path, has_class_directories=True):
-#     dataset = []
-#     path_exp = os.path.expanduser(path)
-#     classes = [path for path in os.listdir(path_exp) if os.path.isdir(os.path.join(path_exp, path))]
-#     classes.sort()
-#     nrof_classes = len(classes)
-#     for i in range(nrof_classes):
-#         class_name = classes[i]
-#         facedir = os.path.join(path_exp, class_name)
-#         image_paths = get_image_paths(facedir)
-#         dataset.append(ImageClass(class_name, image_paths))
-#
-#     return dataset
-
-
-# def get_image_paths(facedir):
-#     image_paths = []
-#     if os.path.isdir(facedir):
-#         images = os.listdir(facedir)
-#         image_paths = [os.path.join(facedir,img) for img in images]
-#     return image_paths
-
-
-# def split_dataset(dataset, split_ratio, min_nrof_images_per_class, mode):
-#     if mode=='SPLIT_CLASSES':
-#         nrof_classes = len(dataset)
-#         class_indices = np.arange(nrof_classes)
-#         np.random.shuffle(class_indices)
-#         split = int(round(nrof_classes*(1-split_ratio)))
-#         train_set = [dataset[i] for i in class_indices[0:split]]
-#         test_set = [dataset[i] for i in class_indices[split:-1]]
-#     elif mode=='SPLIT_IMAGES':
-#         train_set = []
-#         test_set = []
-#         for cls in dataset:
-#             paths = cls.files
-#             np.random.shuffle(paths)
-#             nrof_images_in_class = len(paths)
-#             split = int(math.floor(nrof_images_in_class*(1-split_ratio)))
-#             if split==nrof_images_in_class:
-#                 split = nrof_images_in_class-1
-#             if split>=min_nrof_images_per_class and nrof_images_in_class-split>=1:
-#                 train_set.append(ImageClass(cls.name, paths[:split]))
-#                 test_set.append(ImageClass(cls.name, paths[split:]))
-#     else:
-#         raise ValueError('Invalid train/test split mode "%s"' % mode)
-#     return train_set, test_set
+def restore_checkpoint(saver, session, path):
+    if path is not None:
+        path = Path(path)
+        print('Restoring pre-trained model: {}'.format(path))
+        saver.restore(session, str(path))
 
 
 def load_model(model, input_map=None):
     # Check if the model is a model directory (containing a metagraph and a checkpoint file) or
     # if it is a protobuf file with a frozen graph
 
-    model_exp = pathlib.Path(model).expanduser()
+    model_exp = Path(model).expanduser()
     print('load model: {}'.format(model))
 
     if model_exp.is_file():
