62bd67a8da69a11e9c3c21a4b6d43d32f09412c4
korus <ruslan.kosarev@gmail.com>
2020-02-17 12:09:07 +0300

    refactoring

--- facenet/apps/train_tripletloss.py
+++ facenet/apps/train_tripletloss.py
@@ -323,19 +323,18 @@ def sample_people(dataset, people_per_batch, images_per_person):
     i = 0
     image_paths = []
     num_per_class = []
-    sampled_class_indices = []
 
     # Sample images from these classes until we have enough
     while len(image_paths) < nrof_images:
         class_index = class_indices[i]
         nrof_images_in_class = dataset.classes[class_index].nrof_images
-        image_indices = np.arange(nrof_images_in_class)
-        np.random.shuffle(image_indices)
         nrof_images_from_class = min(nrof_images_in_class, images_per_person, nrof_images - len(image_paths))
-        idx = image_indices[0:nrof_images_from_class]
-        image_paths_for_class = [dataset.classes[class_index].files[j] for j in idx]
-        sampled_class_indices += [class_index] * nrof_images_from_class
-        image_paths += image_paths_for_class
+
+        image_paths_for_class = dataset.classes[class_index].files
+        if nrof_images_in_class > nrof_images_from_class:
+            image_paths_for_class = np.random.choice(image_paths_for_class, size=nrof_images_from_class, replace=False)
+
+        image_paths += list(image_paths_for_class)
         num_per_class.append(nrof_images_from_class)
         i += 1
 
