7344c2c9aa07dab758fe7e19d34f3c452f4ea7c5
korus <ruslan.kosarev@gmail.com>
2020-04-09 18:40:42 +0300

    printing info has been updated

--- facenet/apps/train_softmax.py
+++ facenet/apps/train_softmax.py
@@ -184,14 +184,12 @@ def main(**args_):
                 info = '(model {}, epoch [{}/{}])'.format(args.model.path.stem, epoch+1, args.train.epoch.nrof_epochs)
 
                 # train for one epoch
-                print('\nRunning training', info)
-                train(args, sess, epoch, train_dbase, index_dequeue_op, enqueue_op, placeholders, tensor_dict, train_summary)
+                train(args, sess, epoch, train_dbase, index_dequeue_op, enqueue_op, placeholders, tensor_dict, train_summary, info)
 
                 # perform validation
                 epoch1 = epoch + 1
                 if epoch1 % args.validate.every_n_epochs == 0 or epoch1 == args.train.epoch.nrof_epochs:
-                    print('\nRunning forward pass on validation set', info)
-                    validate(args, sess, epoch, val_dbase, enqueue_op, placeholders, val_tensor_dict, val_summary)
+                    validate(args, sess, epoch, val_dbase, enqueue_op, placeholders, val_tensor_dict, val_summary, info)
 
                 # save variables and the meta graph if it doesn't exist already
                 facenet.save_variables_and_metagraph(sess, saver, args.model.path, epoch)
@@ -220,7 +218,8 @@ def main(**args_):
     return args.model.path
 
 
-def train(args, sess, epoch, dbase, index_dequeue_op, enqueue_op, placeholders, tensor_dict, summary):
+def train(args, sess, epoch, dbase, index_dequeue_op, enqueue_op, placeholders, tensor_dict, summary, info):
+    print('\nRunning training', info)
     start_time = time.monotonic()
 
     learning_rate = facenet.learning_rate_value(epoch, args.train.learning_rate)
@@ -268,7 +267,8 @@ def train(args, sess, epoch, dbase, index_dequeue_op, enqueue_op, placeholders, 
     return True
 
 
-def validate(args, sess, epoch, dbase, enqueue_op, placeholders, tensor_dict, summary):
+def validate(args, sess, epoch, dbase, enqueue_op, placeholders, tensor_dict, summary, info):
+    print('\nRunning forward pass on validation set', info)
     start_time = time.monotonic()
 
     # evaluate batch size and number of batches
