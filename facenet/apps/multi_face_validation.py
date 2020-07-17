1a8249f7d99be9136abe887a32a3545c734a7ce9
korus <ruslan.kosarev@smedx.com>
2020-07-17 15:28:21 +0300

    updates

--- facenet/apps/multi_face_validation.py
+++ facenet/apps/multi_face_validation.py
@@ -17,50 +17,43 @@ def similarity(x, y):
     return 2*(1 - x @ tf.transpose(y))
 
 
-class ConfusionMatrix:
-    def __init__(self, tp, tn, fp, fn):
-        self.accuracy = (tp + tn) / (tp + fp + tn + fn)
-        self.precision = tp / (tp + fp)
-        self.tp_rate = tp / (tp + fn)
-        self.tn_rate = tn / (tn + fp)
-
-
 class Metrics:
     def __init__(self, embeddings, batches, threshold, alpha, far=None):
         nrof_classes = len(embeddings)
         nrof_positive_class_pairs = nrof_classes
         nrof_negative_class_pairs = nrof_classes * (nrof_classes - 1)
 
-        tp = 0
-        tn = 0
-        fp = 0
-        fn = 0
+        tp = tn = fp = fn = 0
         positive_entropy_part = 0
         negative_entropy_part = 0
 
         for i, x in enumerate(embeddings):
             for k, y in enumerate(batches):
-                sims = similarity(x, y)
+                sims = tf.cast(similarity(x, y), dtype=threshold.dtype)
                 logits = tf.multiply(alpha, tf.subtract(threshold, sims))
 
-                predict = tf.greater(logits, 0)
+                predict = tf.greater_equal(logits, 0)
                 mean = tf.count_nonzero(predict) / tf.size(predict, out_type=tf.int64)
 
                 if k == i:
                     logsig = tf.math.log_sigmoid(logits)
+                    positive_entropy_part -= tf.reduce_mean(logsig) / nrof_positive_class_pairs
 
                     tp += mean / nrof_positive_class_pairs
                     fn += (1 - mean) / nrof_positive_class_pairs
-                    positive_entropy_part -= tf.reduce_mean(logsig) / nrof_positive_class_pairs
                 else:
                     logsig = tf.math.log_sigmoid(1 - logits)
+                    negative_entropy_part -= tf.reduce_mean(logsig) / nrof_negative_class_pairs
 
                     fp += mean / nrof_negative_class_pairs
                     tn += (1 - mean) / nrof_negative_class_pairs
-                    negative_entropy_part -= tf.reduce_mean(logsig) / nrof_negative_class_pairs
 
         self.cross_entropy = positive_entropy_part + negative_entropy_part
-        self.conf_matrix = ConfusionMatrix(tp, tn, fp, fn).__dict__
+
+        self.accuracy = (tp + tn) / (tp + fp + tn + fn)
+        self.precision = tp / (tp + fp)
+        self.tp_rate = tp / (tp + fn)
+        self.tn_rate = tn / (tn + fp)
 
 
 @click.command()
@@ -77,29 +70,37 @@ def main(**args_):
     embeddings = facenet.EvaluationOfEmbeddings(dbase, args)
     print(embeddings)
 
+    args = args.multi_face_validation
+
     embeddings = statistics.split_embeddings(embeddings.embeddings, embeddings.labels)
     embeddings = [tf.convert_to_tensor(emb) for emb in embeddings]
 
     # define batches
-    datasets = [tf.data.Dataset.from_tensor_slices(emb).repeat().batch(batch_size=5) for emb in embeddings]
-    batches = [tf.convert_to_tensor(ds.make_one_shot_iterator().get_next()) for ds in datasets]
+    batches = []
+    for emb in embeddings:
+        ds = tf.data.Dataset.from_tensor_slices(emb).shuffle(buffer_size=10, reshuffle_each_iteration=True)
+        ds = ds.repeat().batch(batch_size=10)
+        batches.append(tf.convert_to_tensor(ds.make_one_shot_iterator().get_next()))
 
     # vars and metrics
-    alpha = tf.Variable(initial_value=1, dtype=tf.float32, name='alpha')
-    threshold = tf.Variable(initial_value=1, dtype=tf.float32, name='threshold')
+    alpha = tf.Variable(initial_value=10, dtype=tf.float64, name='alpha')
+    threshold = tf.Variable(initial_value=1, dtype=tf.float64, name='threshold')
 
     metrics = Metrics(embeddings, batches, threshold, alpha)
 
     # define train operations
     global_step = tf.Variable(0, trainable=False, name='global_step')
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
     train_ops = optimizer.minimize(metrics.cross_entropy, global_step=global_step)
+    # ema = tf.train.ExponentialMovingAverage(args.moving_average_decay, global_step)
+    # with tf.control_dependencies([train_ops]):
+    #     train_ops = ema.apply(tf.trainable_variables())
 
-    tensor_ops = {
+    ops = {
+        'train_ops': train_ops,
         'global_step': global_step,
         'loss': metrics.cross_entropy,
-        'alpha': alpha,
-        'threshold': threshold
+        'variables': tf.trainable_variables(),
     }
 
     print('start training')
@@ -107,12 +108,15 @@ def main(**args_):
     with tf.Session() as session:
         session.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
 
-        for _ in tqdm(range(1000)):
-            session.run(train_ops)
+        with tqdm(total=args.nrof_epochs) as bar:
+            for _ in range(args.nrof_epochs):
+                outs = session.run(ops)
 
-        outputs = session.run([tensor_ops, metrics.conf_matrix])
-        for out in outputs:
-            print(out)
+                bar.set_postfix_str('variables {}, loss {}'.format(outs['variables'], outs['loss']))
+                bar.update()
+
+        metrics = Metrics(embeddings, embeddings, threshold, alpha)
+        print(session.run(metrics.__dict__))
 
 
 if __name__ == '__main__':
