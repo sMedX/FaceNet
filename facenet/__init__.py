625630f4dfb3cd74533f658444f395a21454a460
korus <ruslan.kosarev@smedx.com>
2020-09-06 16:15:57 +0300

    update nodes in __init__.py

--- facenet/__init__.py
+++ facenet/__init__.py
@@ -13,17 +13,17 @@ from facenet import tfutils, ioutils
 
 nodes = {
     'input': {
-        'name': ['image'],
+        'name': 'image',
         'type': [dtypes.uint8.as_datatype_enum]
         },
 
     'inference_input': {
-        'name': ['input'],
+        'name': 'input',
         'type': [dtypes.float32.as_datatype_enum]
     },
 
     'output': {
-        'name': ['embedding'],
+        'name': 'embedding',
         'type': [dtypes.float32.as_datatype_enum]
     }
 }
@@ -43,8 +43,8 @@ class FaceNet:
         tfutils.load_model(config.path)
 
         # Get input and output tensors
-        input_node_name = '{}:0'.format(nodes['input']['name'][0])
-        output_node_name = '{}:0'.format(nodes['output']['name'][0])
+        input_node_name = '{}:0'.format(nodes['input']['name'])
+        output_node_name = '{}:0'.format(nodes['output']['name'])
 
         graph = tf.get_default_graph()
         self._phase_train_placeholder = graph.get_tensor_by_name('phase_train:0')
