86f1914e2dc87fa6e1a9fb785113528452788d91
korus <ruslan.kosarev@smedx.com>
2020-09-07 10:17:16 +0300

    small updates

--- torch/slim2torch.py
+++ torch/slim2torch.py
@@ -9,7 +9,8 @@ inp_channels = 3
 out_channels = 32
 kernel_size = (3, 3)
 stride = 2
-slim_padding = 'valid'
+
+slim_padding = 'same'
 
 height = 160
 width = 160
@@ -24,7 +25,7 @@ def apply_slim_conv2d(inputs):
     """
     inputs = tf.convert_to_tensor(inputs)
     net = slim.conv2d(inputs, out_channels, kernel_size=kernel_size, stride=stride,
-                      padding=slim_padding)
+                      padding=slim_padding, activation_fn=None)
 
     with tf.Session() as sess:
         sess.run([tf.compat.v1.global_variables_initializer(), tf.compat.v1.local_variables_initializer()])
@@ -40,21 +41,20 @@ def apply_slim_conv2d(inputs):
 
 def apply_torch_conv2d(inputs, variables):
     if slim_padding == 'same':
-        torch_padding = [s // 2 for s in kernel_size]
+        padding = [s // 2 for s in kernel_size]
     elif slim_padding == 'valid':
-        torch_padding = 0
+        padding = 0
     else:
         raise ValueError()
 
     inputs_torch = torch.from_numpy(np.transpose(inputs, axes=[0, 3, 1, 2]))
     conv2d = torch.nn.Conv2d(inp_channels, out_channels, kernel_size=kernel_size, stride=stride,
-                             padding=torch_padding, padding_mode='zeros', bias=False)
+                             padding=padding, padding_mode='zeros', bias=False)
 
     weight = np.transpose(variables[0], axes=[3, 2, 0, 1])
     conv2d.weight = torch.nn.Parameter(torch.from_numpy(weight))
 
     out = conv2d.forward(inputs_torch)
-    out = torch.nn.ReLU()(out)
 
     out = out.detach().cpu().numpy()
     out = np.transpose(out, axes=[0, 2, 3, 1])
@@ -76,8 +76,9 @@ def main(**options):
     out2 = apply_torch_conv2d(inputs, variables)
     print(out2.shape)
 
-    print(out1[0, :, :, 1])
-    print(out2[0, :, :, 1])
+    idx = [idx//2 for idx in out1.shape]
+    print(out1[idx[0], idx[1]-1:idx[1]+2, idx[2]-1:idx[2]+2, idx[3]])
+    print(out2[idx[0], idx[1]-1:idx[1]+2, idx[2]-1:idx[2]+2, idx[3]])
 
     norm = np.max(np.abs(out1 - out2))
     print('maximum error', norm)
