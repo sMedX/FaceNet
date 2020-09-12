522ef99ebfbd5fa9f6f1560f16ad8d80ed75c387
korus <ruslan.kosarev@smedx.com>
2020-09-12 12:22:09 +0300

    small updates

--- facenet/apps/model_info.py
+++ facenet/apps/model_info.py
@@ -58,11 +58,11 @@ def main(**options):
                 for i, op in enumerate(graph.get_operations()):
                     f.write(f'{i}) {op.name} {op.type}\n')
 
-                    f.write(f'---  inputs {op.inputs}\n')
+                    f.write(f'---  inputs [{len(op.inputs)}] {op.inputs}\n')
                     for input_tensor in op.inputs:
                         f.write(f'            {input_tensor}\n')
 
-                    f.write(f'--- outputs {op.outputs[0]}\n')
+                    f.write(f'--- outputs [{len(op.outputs)}] {op.outputs[0]}\n')
                     for output in op.outputs[1:]:
                         f.write(f'            {output}\n')
 
