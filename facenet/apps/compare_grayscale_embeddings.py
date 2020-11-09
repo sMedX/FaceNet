8c8400a5a6d7c1f99660b888a715f5301d55b0ce
korus <ruslan.kosarev@smedx.com>
2020-11-09 17:48:41 +0300

    update app

---
+++ facenet/apps/compare_grayscale_embeddings.py
@@ -0,0 +1,50 @@
+"""Validate a face recognizer.
+"""
+# MIT License
+#
+# Copyright (c) 2020 SMedX
+
+import click
+from pathlib import Path
+import tensorflow as tf
+
+from facenet import dataset, config, facenet, tfutils, ioutils, DefaultConfig, FaceNet
+import numpy as np
+
+
+@click.command()
+@click.option('--path', default=config.faces_dir,
+              help='Path to directory with extracted faces to evaluate embeddings.')
+@click.option('--model', default=config.default_model, type=Path,
+              help='Path to directory with model.')
+def main(**options):
+
+    # read dataset
+    dbase = dataset.DBase(options['path'])
+    print(dbase)
+
+    # create model
+    conf = DefaultConfig(path=options['model'],
+                         output='embeddings:0')
+                         # output='InceptionResnetV1/Bottleneck/BatchNorm/Reshape_1:0')
+    face_net = FaceNet(conf)
+
+    for file in dbase.files:
+        img1 = ioutils.read_image(file)
+        arr1 = ioutils.pil2array(img1)
+        emb1 = face_net.image_to_embedding(arr1)
+
+        img2 = img1.convert('L')
+        arr2 = ioutils.pil2array(img2)
+        emb2 = face_net.image_to_embedding(arr2)
+
+        diff1 = 2*np.linalg.norm(emb1 - emb2)**2/(np.linalg.norm(emb1)**2 + np.linalg.norm(emb2)**2)
+        diff2 = np.abs(np.linalg.norm(emb1)**2 - np.linalg.norm(emb2)**2) / \
+                      (np.linalg.norm(emb1)**2 + np.linalg.norm(emb2)**2)
+
+        file = Path(file)
+        print(Path(file.parent.name, file.name), diff1, diff2)
+
+
+if __name__ == '__main__':
+    main()
